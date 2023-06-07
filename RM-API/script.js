77e1d491a2b1c5a94f3697c666d3e4779b3f39d4
alessi.gg <alessansdrogar04@gmail.com>
2023-06-07 13:45:07 +0200

    improve aesthetics

--- RM-API/script.js
+++ RM-API/script.js
@@ -1,24 +1,67 @@
-function getCharacters(done){
-    const results = fetch("https://rickandmortyapi.com/api/character");
-    results
-        .then(response => response.json())
-        .then(data =>{
-            done(data)
-        });
-}
+const mainEl = document.querySelector("main");
 
-getCharacters(data => {
-    data.results.forEach(character => {
-        const article = document.createRange().createContextualFragment(/*html*/`
-        <article>
-            <div class="image-container">
-                <img src="${character.image}" alt="character">
-            </div>
-            <h2>${character.name}</h2>
-            <span>${character.status}</span>
-        `);
-        
-        const main = document.querySelector("main");
-        main.append(article);
-    });
-})
+const main = async () => {
+  const characters = await fetchAllCharacters();
+  const maxCells = 6;
+
+  for (let i = 0; i < Math.min(characters.length, maxCells); i++) {
+    const character = characters[i];
+    const episode = await fetchJsonAsync(character.episode[0]);
+    mainEl.insertAdjacentHTML('beforeend', renderCharacterHTML(character, episode));
+  }
+};
+
+const renderCharacterHTML = (character, episode) => `
+  <article class="character-card">
+    <div class="image-container">
+      <img src="${character.image}" alt="Character" height="100%" width="100%">
+    </div>
+    <div class="character-info">
+      <div class="section">
+        <h2>${character.name}</h2>
+        <div class="status">
+          <span class="status-dot ${getStatusColor(character.status)}"></span>
+          <span class="status">${character.status} - ${character.species}</span>
+        </div>
+      </div>
+      <div class="section">
+        <span class="greytext">Last known location:</span>
+        <span>${character.location.name}</span>
+      </div>
+      <div class="section">
+        <span class="greytext">First seen in:</span>
+        <span>${episode.name}</span>
+      </div>
+    </div>
+  </article>
+`;
+
+const getStatusColor = (status) => {
+  switch (status.toLowerCase()) {
+    case 'unknown':
+      return 'grey';
+    case 'dead':
+      return 'red';
+    case 'alive':
+      return 'green';
+    default:
+      return '';
+  }
+};
+
+const fetchAllCharacters = async () => {
+  const response = await fetchJsonAsync('https://rickandmortyapi.com/api/character');
+  return response.results;
+};
+
+const fetchJsonAsync = async (url) => {
+  try {
+    let response = await fetch(url);
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json();
+  } catch (err) {
+    console.error('Error fetching JSON:', err);
+  }
+};
+
+main();
